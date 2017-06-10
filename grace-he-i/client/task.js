36e36303d03eb37e71c95e8281f09e8624a100e8
vmiis <zzm.vmiis@gmail.com>
2017-06-11 08:00:31 +1000

    update

--- grace-he-i/client/task.js
+++ grace-he-i/client/task.js
@@ -1,54 +1,73 @@
 //-------------------------------------
-//input needed in module input variables
-/*
-var m_var=$vm.module_list[$vm.vm['__ID'].name].var;
-var client_pid=m_var.client_tid;
-var sql_client=m_var.sql_client;
-*/
 var sql_client=_sys.config.parameters.sql_client;
 var ids=_sys.config.module_ids;
-var client_pid=_mlist[ids.clients].table_id;
-var document_type_list_tid=_mlist[ids.doc_type_list].table_id;
-//-------------------------------------
-var _task_fields='';
-//-------------------------------------
-_set_req=function(){
-    var client_where="";
-    var client_uid=$vm.vm['__ID'].op.client_uid;
-
-    if(client_uid!==undefined){
-        client_where=" where uid="+client_uid;
-    }
-
-
-    var sql="with client as (select clientUID=UID,sql_client="+sql_client+" from [FORM-"+client_pid+"] "+client_where+" )";
-    sql+=",task as (select ID,UID,PUID,S3,Information,sql_client,DateTime,Author,RowNum=row_number() over (order by ID DESC) from [TABLE-"+_db_pid+"-@S1] join client on PUID=clientUID)";
-    sql+="select ID,S3,UID,Information,client=sql_client,DateTime,Author,RowNum,dirty=0, valid=1 from task where RowNum between @I6 and @I7";
-    var sql_n="with client as (select clientUID=UID from [TABLE-"+client_pid+"] )";
-    sql_n+=" select count(ID) from [TABLE-"+_db_pid+"-@S1] join client on PUID=clientUID";
-
-    _req={cmd:'query_records',sql:sql,sql_n:sql_n,s1:'"'+$('#keyword__ID').val()+'"',I:$('#I__ID').text(),page_size:$('#page_size__ID').val()}
-}
-//-------------------------------------
-_set_req_export=function(){
-/*    _fields_e="client ID|clientUID,client,"+_task_fields
-    var sql="with client as (select Site=S1,clientUID=UID,sql_client="+_module.var.sql_client+",RowNum=row_number() over (order by ID DESC) from [FORM-"+client_pid+"]"+site_sql_where+" )";
-    sql+=",task as (select ID,UID,PUID,S3,Information,DateTime,Author from [FORM-"+_db_pid+"-@S1])";
-    sql+=" select ID,clientUID,Site,Information,client=sql_client,DateTime,Author from client left join task on PUID=clientUID";
-    _set_from_to();
-    if(_from!='0' && _to!='0') sql+=" where RowNum between @I6 and @I7";
-    else sql+=" order by clientUID DESC";
-    _req={cmd:'query_records',sql:sql,i6:_from,i7:_to}
-    */
-}
+var client_pid				=_mlist[ids.clients].table_id;
+var client_tid				=_mlist[ids.clients].table_id;
+var document_type_list_tid	=_mlist[ids.doc_type_list].table_id;
+var job_status_list_tid		=_mlist[ids.job_status].table_id;
+var task_list_tid			=_mlist[ids.task_list].table_id;
+var staff_list_tid			=_mlist[ids.staff_list].table_id;
 //-------------------------------------
 var _default_cell_render=function(records,I,field,td,set_value,source){
     switch(field){
     }
 }
 //-------------------------------------
+_set_req=function(){
+	if(_mobj.op.child=='1'){
+		where=" where puid="+_mobj.op.record.UID;
+		var sql="with tb as (select Information,ID,UID,PUID,DateTime,Author,RowNum=row_number() over (order by ID DESC) from [TABLE-"+_db_pid+"-@S1]"+where+" )";
+	    sql+="select Information,ID,UID,PUID,DateTime,Author,RowNum from tb where RowNum between @I6 and @I7";
+	    var sql_n="select count(ID) from [TABLE-"+_db_pid+"-@S1]"+where;
+	    _req={cmd:'query_records',db_pid:_db_pid,sql:sql,sql_n:sql_n,s1:'"'+$('#keyword__ID').val()+'"',I:$('#I__ID').text(),page_size:$('#page_size__ID').val()}
+	}
+	else{
+		var sql="with tb as (select Information,ID,UID,PUID,DateTime,Author,RowNum=row_number() over (order by ID DESC) from [TABLE-"+_db_pid+"-@S1] )";
+	    sql+="select Information,ID,UID,PUID,DateTime,Author,RowNum from tb where RowNum between @I6 and @I7";
+	    var sql_n="select count(ID) from [TABLE-"+_db_pid+"-@S1]";
+	    _req={cmd:'query_records',db_pid:_db_pid,sql:sql,sql_n:sql_n,s1:'"'+$('#keyword__ID').val()+'"',I:$('#I__ID').text(),page_size:$('#page_size__ID').val()}
+	}
+}
+//-------------------------------------
+_set_req_export=function(){
+	if(_mobj.op.child=='1'){
+		where=" where puid="+_mobj.op.record.UID;
+		var sql="with tb as (select Information,ID,UID,DateTime,Author,RowNum=row_number() over (order by ID DESC) from [TABLE-"+_db_pid+"]"+where+")";
+	    sql+="select Information,ID,UID,DateTime,Author,RowNum from tb";
+	    _set_from_to();
+	    if(_from!='0' && _to!='0') sql+=" where RowNum between @I6 and @I7";
+	    _req={cmd:'query_records',sql:sql,i6:_from,i7:_to}
+	}
+	else{
+		var sql="with tb as (select Information,ID,UID,DateTime,Author,RowNum=row_number() over (order by ID DESC) from [TABLE-"+_db_pid+"] )";
+	    sql+="select Information,ID,UID,DateTime,Author,RowNum from tb";
+	    _set_from_to();
+	    if(_from!='0' && _to!='0') sql+=" where RowNum between @I6 and @I7";
+	    _req={cmd:'query_records',sql:sql,i6:_from,i7:_to}
+	}
+}
+//-------------------------------------
+_data_process_after_render=function(){
+	if(_mobj.op.child=='1' && _mobj.op.single_record=='1'){
+		if(_records.length==0){
+			$('#new__ID').triggerHandler('click');
+		}
+		if(_records.length==1){
+			var form=$('#grid__ID tr:nth-child(2)').find('u:first');
+			form.triggerHandler('click');
+		}
+		else{
+			alert("More than one records were found! Please report the administrator.");
+			return;
+		}
+	}
+}
+//-------------------------------------
 _new_pre_data_process=function(){
-    if($vm.vm['__ID'].op.client_uid!==undefined) _records[0].client_uid=$vm.vm['__ID'].op.client_uid;
-    if($vm.vm['__ID'].op.client_name!==undefined) _records[0].client=$vm.vm['__ID'].op.client_name;
+	if(_mobj.op.child=='1'){
+		var record=_mobj.op.record;
+		_records[0].client_uid=record.UID;
+		_records[0].client=record.Givennames+' '+record.Surname;
+	}
 }
 //-------------------------------------
