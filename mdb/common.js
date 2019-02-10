0fe775cb9460e8fdc3ecced37097741fa0e573a2
Gunnar Unger <gunnar.unger@gmail.com>
2019-02-10 17:11:50 +1100

    update

--- mdb/common.js
+++ mdb/common.js
@@ -1,6 +1,5 @@
 (function(){
     var modules={
-        "about-vmiis":    			    {url:"$A/modules/mdb/a/about-vmiis.html",router:1},
         "notes":  		  	  			{url:"$A/modules/mdb/n/notes.html"},
     }
     for(m in modules){$vm.module_list[m]=modules[m];}
--- mdb/spardac-home.js
+++
@@ -1,6 +0,0 @@
-(function(){
-    var modules={
-        "home":                         {url:"$A/spardacmdb/home.html",router:1},
-    }
-    for(m in modules){$vm.module_list[m]=modules[m];}
-})();
